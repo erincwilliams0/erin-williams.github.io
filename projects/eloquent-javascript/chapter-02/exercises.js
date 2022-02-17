7140a2ca48e572df4ce2ef507923af2363407194
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-02-17 21:38:15 +0000

    eloquentjava ch2 triangles

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -11,7 +11,22 @@ function triangles() {
 ////////////////////////////////////////////////////////////////////////////////
 
 function fizzBuzz() {
-  
+  // create for loop that starts i at 0; stops at 100; increments
+  for(let i = 1; i <= 15; i++) {
+    //use if else if chain to test each iteration for multiples of three, five, both three and five,
+    if((i % 3) === 0 && (i % 5) === 0) {
+        console.log("fizzbuzz")
+    }else if(i % 3 === 0) {
+        // log Fizz
+        console.log("fizz")
+    } else if (i % 5 === 0) {
+        //log Buzz
+        console.log("buzz")
+    } else {
+    // log each number that is not a multiple of three or five
+    console.log(i)
+    }
+}
 }
 
 ////////////////////////////////////////////////////////////////////////////////

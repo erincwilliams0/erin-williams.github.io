dd1cbe7fd428ec7e62adbf26b6a20c2e44e783ff
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-03-07 21:25:55 +0000

    recursion sum

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -5,7 +5,16 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
-};
+  //base
+  if(n < 0){
+    return null;
+  }
+  if(n < 1){
+    return 1;
+  }
+  //recursion
+  return n * factorial(--n)
+}; 
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21

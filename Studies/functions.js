8401974037d095a98f3e81953eedc86a30aa7904
erincwilliams0 <erin.cwilliams0@gmail.com>
2022-03-22 10:02:56 +0000

    studies stillworkinfunctions

--- Studies/functions.js
+++ Studies/functions.js
@@ -5,7 +5,11 @@
  */
 
 // DEFINING AND EXECUTION //
-/* 
+/* Functions are used in two phases, defining a function and executing a function. 
+In order to define a function  you must first input the "function" keywor followed by
+parenthesis, then ending with a function statement or code block wrapped in curly 
+brackets. Executing a function, also known as calling or invoking a function involves
+inputting the function name followed by parenthesis.
 */
 
 function print1() {
@@ -15,7 +19,10 @@ function print1() {
 print1(); // => calls, executes, or invokes the function => prints 1
 
 // PARAMETERS VS ARGUMENTS //
-/* 
+/* During function declaration parameters are stored in the parenthesis following the
+function name. Parameters are named place holders indicating inputs that our function may
+operate on. When invoking a function, arguments are placed in parenthesis after the
+function name. 
 */
 
 function sum(x, y){
@@ -24,7 +31,7 @@ function sum(x, y){
 sum(3, 2); // => 3, 2 are arguments passed to the function sum
 
 // FUNCTION DECLARATION //
-/* 
+/* All function declarations are named functions. 
 */
 
 function declare() {
